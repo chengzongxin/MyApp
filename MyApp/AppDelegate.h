d9d65de99cb1dcdf040940c2237cd2fbd6487fa3
Jason <8876142@qq.com>
2018-05-28 15:53:26 +0800

    Initial Commit

---
+++ MyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by Jason on 2018/5/28.
+//  Copyright © 2018年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyApp
+//
+//  Created by Jason on 2018/5/28.
+//  Copyright © 2018年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
